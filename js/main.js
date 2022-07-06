83702e61c9d6ba0da3b280dfe4fe11e2a9b42f6e
Pauline.B <paulinebanye@gmail.com>
2022-07-06 13:04:52 +0100

    minor edits

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ userAge >= 18
 : console.log(`You are left with ${18 - userAge} years to drive.`);
 
 
-
-// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
-// Enter your age: 30
-// You are 5 years older than me.
+/* 2 -  Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+Enter your age: 30
+You are 5 years older than me.
+*/
 
 // prompt for input
 let myAge = prompt("Enter my age");
